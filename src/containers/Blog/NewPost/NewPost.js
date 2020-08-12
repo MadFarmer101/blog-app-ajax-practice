8f4b8136dffdc89c401d264dc3b53f5f1050138c
Janko <mailodjanka@gmail.com>
2020-08-12 13:06:55 +0200

    Uses to replace instead of push to change to /posts after submitting a new post

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -17,7 +17,7 @@ class NewPost extends Component {
       author: this.state.author,
     };
     axios.post("/", post).then((response) => console.log(response));
-    this.props.history.push("/posts");
+    this.props.history.replace("/posts");
   };
 
   render() {
